85eb0ef2436ebaa0e56f91b2042905c28f557206
Chris Bell <chrislbellny@gmail.com>
2015-08-06 10:19:04 -0400

    Finished binary Int, working for positive and negative ints

--- Programming Practice/src/Integers/BinaryInt.java
+++ Programming Practice/src/Integers/BinaryInt.java
@@ -4,13 +4,29 @@ import java.util.*;
 public class BinaryInt {
 
 	public static void main(String[] args) {
-
+		BinaryInt twenty = new BinaryInt(20);
+		BinaryInt fourMillion = new BinaryInt(4000000);
+		BinaryInt negativeNum = new BinaryInt(-2347891);
+	}
+	
+	BinaryInt(int x){
+		bitVector = "";
+		
+		unsignedDecimalToBinary(x);
+		padWithZeros();
+		
+		if(x < 0)
+			unsignedToSigned();
+		
+		System.out.println(x + " in binary: " + bitVector);
 	}
 
 	static String bitVector = "";
 	
 	static void unsignedDecimalToBinary(int x){
 		Stack<Integer> bits = new Stack<Integer>();
+		
+		bitVector = "";
 		
 		int num = Math.abs(x);
 		
@@ -32,8 +48,44 @@ public class BinaryInt {
 				bitVector += "0";
 		}
 	}
+	
+	static void padWithZeros(){
+		while(bitVector.length() < 32){
+			bitVector = "0" + bitVector;
+		}
+	}
 
-	static void unsignedToSigned(){
+	static void unsignedToSigned(){	//flip the bits then add 1
+		bitVector = bitVector.replace('0', '2'); //dummy placeholder
+		bitVector = bitVector.replace('1', '0');
+		bitVector = bitVector.replace('2', '1');
+		
+		
+		String[] bits = bitVector.split("");
+		int index = bits.length - 1;		
+		
+		while(true){
+			if(bits[index].equals("0")){
+				bits[index] = "1";
+				break;
+			}
+			
+			index--;
+		}
+		
+		bitVector = "";
+		
+		for(int i= 0; i < bits.length; i++){
+			bitVector += bits[i];
+		}
+		
+		/*
+		 * in this binary addition, we needn't worry about carries simply because we're adding 1. 
+		 * It's implied that we start with a "carry" and that it will be dumped into the first 
+		 * zero found in the bit vector. Once we find that, there can't be any further carries, so we stop.
+		 */
 		
 	}
+
+
 }
--- Programming Practice/src/Integers/BinaryIntTest.java
+++ Programming Practice/src/Integers/BinaryIntTest.java
@@ -14,8 +14,22 @@ public class BinaryIntTest {
 	}
 	
 	@Test
-	public void unsignedToSignedTest(){
-		String bit = "01101";
+	public void padWithZerosTest(){
+		int num = 233;
+		BinaryInt.unsignedDecimalToBinary(num);
+		BinaryInt.padWithZeros();
+		assertEquals("00000000000000000000000011101001", BinaryInt.bitVector);
 	}
+	
+	@Test
+	public void unsignedToSignedTest(){
+		int start = -13;	
+		BinaryInt.unsignedDecimalToBinary(start);
+		BinaryInt.padWithZeros();
+		BinaryInt.unsignedToSigned();
+		assertEquals("11111111111111111111111111110011", BinaryInt.bitVector);
+	}
+	
+	
 
 }

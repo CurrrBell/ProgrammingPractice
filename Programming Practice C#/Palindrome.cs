9bdd087102b7254d0d2fc3e39ea95c09948a55a8
CurrrBell <chrislbellny@gmail.com>
2015-09-01 13:15:11 -0400

    Completed palindrome exercise

---
+++ Programming Practice C#/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingProjectCSharp
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            String test1 = "racecar";
+            String test2 = "not a palindrome";
+
+            Console.WriteLine(isPalindrome(test1));
+            Console.WriteLine(isPalindrome(test2));
+
+        }
+
+        static bool isPalindrome(string s)
+        {
+            char[] chars = s.ToCharArray();
+            int end = chars.Length-1;
+
+            for (int i = 0; i < chars.Length / 2; i++)
+            {
+                if (chars[i] != chars[end - i])
+                    return false;
+            }
+
+                return true;
+        }
+    }
+}
